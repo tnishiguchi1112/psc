aafd567b2f6526a21177b7997a6e7b9862d79352
Takato Nishiguchi <t.nishiguchi1112@gmail.com>
2023-01-29 17:51:30 +0900

    1st commit

--- Denomination_quantity_calculation_tool.py
+++ Denomination_quantity_calculation_tool.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+# git init
+# git remoto add origin https://github.com/tnishiguchi1112/psc.git
+# git add .
+# git commit -m '1st commit'
+# git push origin main
+
 st.title('金種枚数計算ツール')
 
 TBL = [10000, 5000, 1000, 500, 100, 50, 10 , 5 , 1] #金種表
